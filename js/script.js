6f36898c77e1899ecfd8ae564fe6af1b146001a3
MartaStach <marta.m.stach@gmail.com>
2022-09-20 19:26:50 +0200

    sass added, classes 'active' removed

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /*[Done] remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+ 
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
-  /* add class 'active' to the clicked link */
+  /* [in progress] add class 'active' to the clicked link */
+  console.log('clickedElement', clickedElement);
 
-  /* remove class 'active' from all articles */
+  /* [Done]-remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('article.active');
+  console.log(activeArticles)
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
+
 
   /* get 'href' attribute from the clicked link */
 
