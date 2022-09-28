db203ffd882e87f76fc668b65e5a0c1774b0b84f
MartaStach <marta.m.stach@gmail.com>
2022-09-28 17:37:59 +0200

    generator of links added

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@
 function titleClickHandler(event) {
   event.preventDefault();
   const clickedElement = this;
+  generateTitleLinks();
 
   /*[Done] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
- 
+
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
@@ -23,7 +24,7 @@ function titleClickHandler(event) {
 
   /* [done] get 'href' attribute from the clicked link */
   let clickedArticleHref = this.getAttribute('href');
-  
+
   /* [done] find the correct article using the selector (value of 'href' attribute) */
   let clickedArticle = document.querySelector(clickedArticleHref);
 
@@ -32,8 +33,26 @@ function titleClickHandler(event) {
 
 }
 
-const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListelector = '.titles';
+
+function generateTitleLinks() {
+  const titleList = document.querySelector(optTitleListelector);
+  titleList.innerHTML = '';
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+  for (let article of articles) {
+    const articleID = article.getAttribute('id');
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    const linkHTML = '<li><a href="#' + articleID + '"><span>' + articleTitle + '</span></a></li>';
+    html = html + linkHTML
+  }
+  titleList.innerHTML = html;
+
+  const links = document.querySelectorAll('.titles a');
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
 }
